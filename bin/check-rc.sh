aebf91a461e165774f2a3ec87ce7205b28030639
David J. H. Shih <djh.shih@gmail.com>
2017-10-26 15:35:07 -0400

    Fixed typo in shebang line. Avoid using special conditional statement.

--- bin/check-rc.sh
+++ bin/check-rc.sh
@@ -1,4 +1,4 @@
-#/!/bin/bash
+#!/bin/bash
 
 root=$1
 
@@ -7,12 +7,12 @@ final_rc=0
 for f in $(find $1 -name rc); do
 	echo ${f#*/} $(cat $f);
 	rc=$(cat $f)
-	if (( $rc != 0 )); then
+	if [[ ! $rc -eq 0 ]]; then
 		final_rc=1
 	fi
 done
 
-if (( $final_rc == 0 )); then
+if [[ $final_rc -eq 0 ]]; then
 	echo "overall status: success"
 else
 	echo "overall status: failure"
