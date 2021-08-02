12ecdf62da0adee37db48e57655467de36c223f9
David J. H. Shih <djh.shih@gmail.com>
2021-08-01 23:56:39 -0400

    Update cromwell version.

--- bin/install-cromwell.sh
+++ bin/install-cromwell.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 set -e
 
-# install cromwell
+# install cromwell and womtool
 
-name=cromwell
-version="29"
-
+names=(cromwell womtool)
+version="66"
 author=broadinstitute
 repo=cromwell
-url=https://github.com/$author/$repo/releases/download/${version}/${name}-${version}.jar
+url=https://github.com/$author/$repo/releases/download/${version}
 
+for name in ${names[@]}; do
 
-# set the installation directory
+	# set the installation directory
 
-target_dir=${DESTDIR:-$HOME/.cromwell}
-echo "Installing to $target_dir ..."
+	target_dir=${DESTDIR:-$HOME/.cromwell}
+	echo "Installing to $target_dir ..."
 
-# download source files for tmux, libevent, and ncurses
-# save them in /tmp
+	# download source files for tmux, libevent, and ncurses
+	# save them in /tmp
 
-mkdir -p $target_dir/{bin,jar} && cd $target_dir
+	mkdir -p $target_dir/{bin,jar} && cd $target_dir
 
-wget -O jar/${name}-${version}.jar $url
+	wget -O jar/${name}-${version}.jar $url/${name}-${version}.jar
 
-jar_path=$(readlink -f jar/${name}-${version}.jar)
-echo "java -Xmx1G -XX:+UseSerialGC -jar $jar_path \"\$@\"" > bin/$name
-chmod +x bin/$name
+	jar_path=$(readlink -f jar/${name}-${version}.jar)
+	echo "java -Xmx1G -XX:+UseSerialGC -jar $jar_path \"\$@\"" > bin/$name
+	chmod +x bin/$name
 
-cd -
+	cd -
+
+done
